026b3968d6473c7668c347e60555ca8ad7f6ae9c
gson Liang <yuanyu90221@gmail.com>
2024-03-09 22:17:10 +0800

    feat(add serializer, repository): add serializer, repository
    add serializer, repository for port layer

---
+++ internal/serializer/msgpack/serializer.go
@@ -0,0 +1,27 @@
+package msgpack
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/leetcode-golang-classroom/golang-url-shorter/internal/shortener"
+	"github.com/vmihailenco/msgpack"
+)
+
+type Redirect struct{}
+
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+	if err := msgpack.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	rawMsg, err := msgpack.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serilizer.Redirect.Encode")
+	}
+	return rawMsg, nil
+}
